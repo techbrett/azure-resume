3a83ebad99256b98b4299dd256cc287f7a2dd6df
techbrett <brett.hagenauer@gmail.com>
2023-09-24 16:19:48 -0400

    changes to main.js file

--- frontend/main.js
+++ frontend/main.js
@@ -1,23 +1,25 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+// Wrap your code in a DOMContentLoaded event listener
+document.addEventListener('DOMContentLoaded', () => {
+    // Your code here
     getVisitCount();
 });
 
-
 const localApi = 'http://localhost:7071/api/UpdateVisitorCount';
-const functionApi = 'https://pythoncounter.azurewebsites.net/api/UpdateVisitorCount?code=CV2o1cNOLDRT-jtkF5p_DcOpQe-jpO6vi-_QKBOjul6_AzFul9thKQ=='; 
+const functionApi = 'https://pythoncounter.azurewebsites.net/api/UpdateVisitorCount?code=CV2o1cNOLDRT-jtkF5p_DcOpQe-jpO6vi-_QKBOjul6_AzFul9thKQ==';
 
 const getVisitCount = () => {
     let count = 0;
     fetch(functionApi)
     .then(response => {
-        return response.json()
+        return response.json();
     })
     .then(response => {
         console.log("Website called function API.");
         count = response.count;
         document.getElementById('counter').innerText = count;
-    }).catch(function(error) {
+    })
+    .catch(function(error) {
         console.log(error);
-      });
+    });
     return count;
 }
